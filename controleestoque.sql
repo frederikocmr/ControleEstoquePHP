f9b2a63f64f3ac36d67e803dcc80aca166d6c7f0
Anna Lara <annalara1426@gmail.com>
2018-12-11 00:24:59 -0200

    Banco de dados atualizado

--- controleestoque.sql
+++ controleestoque.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Dec 10, 2018 at 04:05 AM
--- Server version: 5.7.23
--- PHP Version: 7.2.10
+-- Generation Time: 11-Dez-2018 às 02:23
+-- Versão do servidor: 5.7.23
+-- versão do PHP: 5.6.38
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,21 +25,79 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `grupo_produto`
+-- Estrutura da tabela `grupo_produto`
 --
 
 DROP TABLE IF EXISTS `grupo_produto`;
 CREATE TABLE IF NOT EXISTS `grupo_produto` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(60) NOT NULL,
-  `description` varchar(100) NOT NULL,
+  `descricao` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=53 DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `grupo_produto`
+--
+
+INSERT INTO `grupo_produto` (`id`, `nome`, `descricao`) VALUES
+(28, 'Vasilha', 'vasilha de aluminio'),
+(29, 'lÃ¡pis', 'lÃ¡pis rosa'),
+(30, 'lÃ¡pis', 'lÃ¡pis rosa'),
+(31, 'lÃ¡pis', 'lÃ¡pis rosa'),
+(32, 'lÃ¡pis', 'lÃ¡pis rosa'),
+(33, 'lÃ¡pis', 'lÃ¡pis rosa'),
+(34, 'lÃ¡pis', 'lÃ¡pis rosa'),
+(35, 'lÃ¡pis', 'lÃ¡pis rosa'),
+(36, 'lÃ¡pis', 'lÃ¡pis rosa'),
+(37, 'teste', '1256sss'),
+(38, 'teste', '1256sss'),
+(39, 'teste', '1256sss'),
+(40, 'teste', '1256sss'),
+(41, 'teste', '1256sss'),
+(42, 'teste', '1256sss'),
+(43, 'teste', '1256sss'),
+(44, 'teste', '1256sss'),
+(45, '', ''),
+(46, '', ''),
+(47, '', ''),
+(48, '', ''),
+(49, '', ''),
+(50, '', ''),
+(51, '', ''),
+(52, '', '');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Estrutura da tabela `produto`
+--
+
+DROP TABLE IF EXISTS `produto`;
+CREATE TABLE IF NOT EXISTS `produto` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(60) DEFAULT NULL,
+  `descricao` varchar(100) DEFAULT NULL,
+  `id_grupo` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_grupo` (`id_grupo`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `produto`
+--
+
+INSERT INTO `produto` (`id`, `nome`, `descricao`, `id_grupo`) VALUES
+(1, 'Camisa', 'Camisa rosa com bolso', 2),
+(2, 'Blusa', 'Blusa laranja decote', 1),
+(3, 'CalÃ§a', 'CalÃ§a preta com ziper', 3),
+(4, 'oios', 'sasdsd', 31),
+(5, 'tese', 'sdasd', 44);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `users`
 --
 
 DROP TABLE IF EXISTS `users`;
@@ -50,17 +108,18 @@ CREATE TABLE IF NOT EXISTS `users` (
   `user_type` varchar(20) NOT NULL,
   `password` varchar(100) NOT NULL,
   PRIMARY KEY (`id`,`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COMMENT='Tabela de Usuários';
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COMMENT='Tabela de Usuários';
 
 --
--- Dumping data for table `users`
+-- Extraindo dados da tabela `users`
 --
 
 INSERT INTO `users` (`id`, `username`, `email`, `user_type`, `password`) VALUES
 (1, 'admin', 'admin@admin.com', 'admin', 'e10adc3949ba59abbe56e057f20f883e'),
 (7, 'fred', 'fred@email.com', 'user', '570a90bfbf8c7eab5dc5d4e26832d5b1'),
 (8, 'anna', 'anna@email.com', 'user', 'e10adc3949ba59abbe56e057f20f883e'),
-(9, 'urubu', 'urubu@enasi.com', 'admin', 'e10adc3949ba59abbe56e057f20f883e');
+(9, 'urubu', 'urubu@enasi.com', 'admin', 'e10adc3949ba59abbe56e057f20f883e'),
+(10, 'Anna Lara', 'annalara1426@gmail.com', 'user', 'e10adc3949ba59abbe56e057f20f883e');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
